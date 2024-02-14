8a8966f3358f6e0d41ae203e9638bbf8eeb98e55
Jose Diaz <jose.diaz.roviralta@outlook.es>
2024-02-14 12:30:37 +0100

    throttle and test working

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,27 +1,33 @@
 import { throttle } from "../index"
+
 const delay: number = 500
+const func = jest.fn( (nombre: string) => {
+  console.log("Hola " + nombre)
+})
+const throttledFunc = throttle(func, delay)
 
 jest.useFakeTimers()
 
 describe('myFunction throttle', () => {
-  it('should throttle the function calls', () => {
-    const func = jest.fn();
-    const throttledFunc = throttle(func, delay)
+  it('should only be able to call the function once the execution time has passed', () => {
+    throttledFunc("Pepito")
+
+    jest.advanceTimersByTime(250)
+
+    throttledFunc("Juan")
+
+    jest.advanceTimersByTime(250)
+    expect(func).toHaveBeenCalledTimes(1)
     
-    throttledFunc()
-    jest.advanceTimersByTime(500)
-    expect(func).toHaveBeenCalledTimes(1)
+    throttledFunc("Jose")
+    throttledFunc("Alverto")
+    throttledFunc("Juanjo")
 
-    throttledFunc()
-    jest.advanceTimersByTime(200)
-    expect(func).toHaveBeenCalledTimes(1)
+    jest.advanceTimersByTime(250)
 
-    jest.advanceTimersByTime(300)
+    throttledFunc("Sara")
+
+    jest.advanceTimersByTime(250)
     expect(func).toHaveBeenCalledTimes(2)
-
-    throttledFunc()
-    jest.advanceTimersByTime(500)
-    jest.runAllTimers()
-    expect(func).toHaveBeenCalledTimes(3)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,12 @@
-export function throttle(func: Function, ms: number): any  {
-  let waitToEnd: boolean = false
-  return function (...args: any){
-    if (!waitToEnd){
-      waitToEnd = true
-      setTimeout( () => {
-        func(...args)
-        waitToEnd = false
-      }, ms)
-    } 
-    else {
-      setTimeout(() => {
-        throttle(func, ms)
-      }, ms)
+export function throttle(func: Function, ms: number){
+  let lastExecution: number = 0
+
+  return function (...args: any[]){
+    const now: number = Date.now()
+
+    if (now - lastExecution >= ms){
+      func(...args)
+      lastExecution = now
     }
   }
 }
-
-/*throttle(throttle, 500, "Hola")
-throttle(throttle, 1000, "Adios")
-throttle(throttle, 1500, "Hola de nuevo")
-throttle(throttle, 2000, "Bueno ahora si que ya me voy a ir yendo, adios")*/
